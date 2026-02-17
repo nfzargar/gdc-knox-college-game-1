32d514b99ef6e90cc5b4379c98647d132d3db481
David Sulpice <DJsulpice123@gmail.com>
2026-02-17 17:29:05 -0600

    fixed interaction areas

--- gdc-game-1/scripts/components/interaction_area.gd
+++ gdc-game-1/scripts/components/interaction_area.gd
@@ -2,27 +2,36 @@ extends Area2D
 class_name InteractionArea
 
 @export var input_binding : String
+var areas_inside : Array[InteractableArea] = []
+
 
 func _process(delta: float) -> void:
 	# Handle Interactions
 	if(Input.is_action_pressed(input_binding)):
 		_activate_interactive_areas()
-	else:
+	elif(Input.is_action_just_released(input_binding)):
 		_deactivate_interactive_areas()
-	var areas_inside = false
-	for area in get_overlapping_areas():
-		if(area is InteractableArea):
-			if(area.show_prompt && !area.activated_since_entering):
-				areas_inside = true
-			if(!area.is_inside):
-				area.enter()
+
 
 func _activate_interactive_areas():
-	for area in get_overlapping_areas():
+	for area in areas_inside:
 		if(area is InteractableArea):
 			area.activate()
-			
+
+
 func _deactivate_interactive_areas():
-	for area in get_overlapping_areas():
+	for area in areas_inside:
 		if(area is InteractableArea and area.holdable and area.activated_since_entering):
 			area.deactivate()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if(area is InteractableArea):
+		areas_inside.append(area)
+		area.enter()
+
+
+func _on_area_exited(area: Area2D) -> void:
+	if(area is InteractableArea && area in areas_inside):
+		areas_inside.erase(area)
+		area.exit()
